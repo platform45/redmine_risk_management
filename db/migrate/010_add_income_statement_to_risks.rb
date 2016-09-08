c39d6e9ed59ef9f731481e5c88eacc36860edfe2
Christopher Swan <christopherswan2005@gmail.com>
2016-09-08 10:40:38 +0200

    patch: Remove redundant comment

--- db/migrate/010_add_income_statement_to_risks.rb
+++ db/migrate/010_add_income_statement_to_risks.rb
@@ -1,5 +1,3 @@
-# The AddControlsToRisk migration will add a controls text item to the risk
-# model allowing the risk creator to spesify how to prevent the risk
 class AddIncomeStatementToRisks < ActiveRecord::Migration
   def change
     add_column :risks, :income_statement, :text
